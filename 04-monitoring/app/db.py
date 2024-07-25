5e712934be5b2fba92799847f94298a639928734
Adnane Aabbar <adnaneaabbar@gmail.com>
2024-07-26 01:38:10 +0200

    adding grafana for system monitoring

--- 04-monitoring/app/db.py
+++ 04-monitoring/app/db.py
@@ -9,7 +9,7 @@ tz = ZoneInfo("Europe/Paris")
 
 def get_db_connection():
     return psycopg2.connect(
-        host=os.getenv("POSTGRES_HOST", "postgres"),
+        host=os.getenv("POSTGRES_HOST", "localhost"),
         database=os.getenv("POSTGRES_DB", "course_assistant"),
         user=os.getenv("POSTGRES_USER", "admin"),
         password=os.getenv("POSTGRES_PASSWORD", "admin"),
